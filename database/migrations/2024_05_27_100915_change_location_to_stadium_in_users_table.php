fc5b5418b071327a80bf39602fbe9af35c1fea4a
Kuzman <fkuzmanovic9@gmail.com>
2024-05-27 12:12:31 +0200

    Dodate nove migracije

--- database/migrations/2024_05_27_100915_change_location_to_stadium_in_users_table.php
+++ database/migrations/2024_05_27_100915_change_location_to_stadium_in_users_table.php
@@ -13,14 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('events', function (Blueprint $table) {
+            $table -> renameColumn('location', 'stadium');
         });
     }
 
@@ -31,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('events', function (Blueprint $table) {
+            $table -> renameColumn('stadium', 'location');
+        });
     }
 };
