5086444ae3c90623d6014ad48c9e44147e50858f
Aleksa Lukic <a.lukic00@gmail.com>
2024-06-18 19:58:23 +0200

    Inicijalni commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EventsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
