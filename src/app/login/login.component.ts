e5bcf24f8b572963ce17a72aef5903f7ab2615a0
Aleksa Lukic <a.lukic00@gmail.com>
2024-06-28 17:57:20 +0200

    Ubacena logika registracije

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,63 +9,50 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  isLoading: boolean = false;
-  error: string = "";
   isSignUp = false;
-  isLoggedIn: boolean = false;
+  isLoading = false;
+  error: string | null = null;
 
   constructor(private authService: AuthService, private router: Router) { }
 
   onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
     const email = form.value.email;
     const password = form.value.password;
-    const name = form.value.name;
 
     this.isLoading = true;
 
     if (this.isSignUp) {
-      this.authService.signUp(email, password, name).subscribe(
-        (data: any) => {
-          console.log(data);
+      const name = form.value.name;
+      this.authService.signUp(name, email, password).subscribe({
+        next: () => {
           this.isLoading = false;
-          this.router.navigate(['/events']); // Preusmeravanje na events stranicu
+          this.router.navigate(['/']);
         },
-        (error) => {
-          console.error(error);
+        error: (errMessage) => {
+          this.error = errMessage;
           this.isLoading = false;
-          this.error = error.message; // Prikazivanje greške ako postoji
         }
-      );
+      });
     } else {
-      this.authService.logIn(email, password).subscribe(
-        (data: any) => {
-          console.log(data);
+      this.authService.logIn(email, password).subscribe({
+        next: () => {
           this.isLoading = false;
-          this.router.navigate(['/events']); // Preusmeravanje na events stranicu
+          this.router.navigate(['/']);
         },
-        (error) => {
-          console.error(error);
+        error: (errMessage) => {
+          this.error = errMessage;
           this.isLoading = false;
-          this.error = error.message; // Prikazivanje greške ako postoji
         }
-      );
+      });
     }
 
     form.reset();
   }
-  logout() {
-    this.authService.logOut(); // Metoda za odjavljivanje
-    this.router.navigate(['/events']); // Preusmeravanje na events stranicu nakon odjavljivanja
-    this.isLoggedIn = false; // Ažuriranje statusa prijavljenosti
-  }
+
   switchButtonClicked() {
     this.isSignUp = !this.isSignUp;
-    if (this.isSignUp) {
-      document.getElementById("loginButton")!.innerText = "Sign Up";
-      document.getElementById("switchButton")!.innerText = "Switch to Login";
-    } else {
-      document.getElementById("loginButton")!.innerText = "Login";
-      document.getElementById("switchButton")!.innerText = "Switch to Sign Up";
-    }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import { AuthResponseData } from '../models/user';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private signupUrl = 'http://127.0.0.1:8000/api/users';
+  private signupUrl = 'http://127.0.0.1:8000/api/register';
   private loginUrl = 'http://127.0.0.1:8000/api/login';
   private logoutUrl = 'http://127.0.0.1:8000/api/logout';
 
@@ -19,64 +18,50 @@ export class AuthService {
     return this.http.post<any>(this.signupUrl, { name, email, password }).pipe(
       tap(
         (resData: any) => {
-          this.handleAuthentication(resData.email, resData.userId, resData.token, resData.expiresIn);
+          this.handleAuthentication(resData.user.email, resData.user.id, resData.token);
         }
       ),
       catchError(this.handleError)
     );
   }
 
-  logIn(email: string, password: string): Observable<AuthResponseData> {
-    return this.http.post<AuthResponseData>(this.loginUrl, { email, password }).pipe(
+  logIn(email: string, password: string): Observable<any> {
+    return this.http.post<any>(this.loginUrl, { email, password }).pipe(
       tap(
-        (resData: AuthResponseData) => {
-          if (resData && resData.token) {
-            this.handleAuthentication(resData.email, resData.userId, resData.token, resData.expiresIn);
-          } else {
-            console.error('Invalid server response after login:', resData);
-          }
-        },
-        (error: any) => {
-          console.error('Error logging in:', error);
-          if (error.status === 422) {
-            const errorMessage = 'Invalid credentials. Please try again.';
-            return throwError(errorMessage);
-          } else {
-            return throwError('Unknown error occurred during login.');
-          }
+        (resData: any) => {
+          this.handleAuthentication(resData.user.email, resData.user.id, resData.token);
         }
-      )
+      ),
+      catchError(this.handleError)
     );
   }
 
   logOut(): Observable<any> {
     return this.http.post<any>(this.logoutUrl, {}).pipe(
       tap(() => {
-        this.clearAuthentication(); // Clear authentication data locally
-        this.router.navigate(['/login']); // Redirect to login page after logout
+        this.clearAuthentication();
+        this.router.navigate(['/login']);
       })
     );
   }
 
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('token'); // Check if token exists in local storage
+    return !!localStorage.getItem('token');
   }
 
-  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
-    localStorage.setItem('token', token); // Example: Store token in local storage
+  private handleAuthentication(email: string, userId: string, token: string) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('userEmail', email);
+    localStorage.setItem('userId', userId);
   }
 
   private handleError(error: HttpErrorResponse) {
-    console.error('Error occurred:', error);
     let errorMessage = 'Unknown error occurred.';
     if (error.error instanceof ErrorEvent) {
-      // Client-side error
       errorMessage = `Error: ${error.error.message}`;
     } else {
-      // Server-side error
       errorMessage = this.extractErrorMessage(error);
     }
-    console.error(errorMessage);
     return throwError(errorMessage);
   }
 
@@ -91,6 +76,8 @@ export class AuthService {
   }
 
   private clearAuthentication() {
-    localStorage.removeItem('token'); // Example: Remove token from local storage
+    localStorage.removeItem('token');
+    localStorage.removeItem('userEmail');
+    localStorage.removeItem('userId');
   }
 }
